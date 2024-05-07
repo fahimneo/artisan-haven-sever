5d0be7cc4c2dea0b5940d80059c0ec7d83671fad
Rakibul Islam <eb191060@ugrad.iiuc.ac.bd>
2024-05-08 05:49:57 +0600

    All Routes Update and Extra section Added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
@@ -30,6 +30,7 @@ async function run() {
     // await client.connect();
 
         const craftCollection = client.db("artisanHaven").collection("crafts");
+        const extraCollection = client.db("artisanHaven").collection("extraSection");
 
         app.post('/addCraft', async (req, res) => {
             const newCraft = req.body;
@@ -37,19 +38,27 @@ async function run() {
             const result = await craftCollection.insertOne(newCraft);
             res.send(result);
             });
+        app.get('/extraItem', async (req, res) => {
+              const cursor = extraCollection.find();
+              const results = await cursor.toArray();
+              console.log(results);
+              res.send(results);
+            });
 
-        app.get('/addCraft', async (req, res) => {
-            const cursor = craftCollection.find({});
-            const results = await cursor.toArray();
-            console.log(results);
-            res.send(results);
-            });
+        app.get('/addCraft', async ( req , res) => {
+          const cursor = craftCollection.find({});
+          const results = await cursor.toArray();
+          console.log(results);
+          res.send(results);
+        });
         app.get('/addCraft/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await craftCollection.findOne(query);
-            res.send(result);
-            });
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const result = await craftCollection.findOne(query);
+          res.send(result);
+        });
+
+        
                 
         app.delete('/deleteItem/:id', async (req, res) => {
             const result = await craftCollection.deleteOne({ _id: new ObjectId(req.params.id) });
@@ -75,81 +84,13 @@ async function run() {
             const result = await craftCollection.updateOne(query, { $set: updatedCraft });
             res.send(result);
         });
-
-        
-        
-
-
-
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // Establish and verify connection
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kk1ndjs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     // await client.connect();
-
-//       const craftCollection = client.db("gradientGallery").collection("crafts");
-//       const extraCollection = client.db("gradientGallery").collection("extra");
-
-//       app.get('/extraItem', async (req, res) => {
-//         const cursor = extraCollection.find();
-//         const results = await cursor.toArray();
-//         console.log(results);
-//         res.send(results);
-//       });
-
-//       app.get('/addCraft/:id', async (req, res) => {
-//         const id = req.params.id;
-//         const query = { _id: new ObjectId(id) };
-//         const result = await craftCollection.findOne(query);
-//         res.send(result);
-//       });
-
-//       
-      
-//       
-
-//       app.put('/addCraft/:id', async (req, res) => {
-//         const id = req.params.id;
-//         const updatedCraft = req.body;
-//         const query = { _id: new ObjectId(id) };
-//         const result = await craftCollection.updateOne(query, { $set: updatedCraft });
-//         res.send(result);
-//       });
-
-
-//     // Send a ping to confirm a successful connection
-//     // await client.db("admin").command({ ping: 1 });
-//     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
 
 
 // Routes
